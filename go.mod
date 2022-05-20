23fce3c1a98e46527945d672fa4ed9aaad6cc763
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-05-20 19:03:01 +0700

    add feature C bycrpt golang

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gozzafadillah
 
 go 1.17
+
+require golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main() {
-	fmt.Println("hehehehe")
-	fmt.Println("hello world")
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
 
-func FeatureA() {
-	fmt.Println("feature a")
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
+func main() {
+	password := "!Ciba1234"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
 }
