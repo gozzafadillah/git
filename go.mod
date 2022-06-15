b1a5c316f32cc246ff171686940e7a6e43be65cd
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-15 23:00:28 +0700

    succes add slug

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module gozzafadillah
 go 1.17
 
 require github.com/xlzd/gotp v0.0.0-20220110052318-fab697c03c2c
+
+require (
+	github.com/gosimple/slug v1.12.0 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,70 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"math/rand"
-	"net/http"
 
-	"github.com/xlzd/gotp"
+	"github.com/gosimple/slug"
 )
 
-type respon struct {
-	Messages []struct {
-		Status string `json:"Status"`
-		To     []struct {
-			Email       string `json:"Email"`
-			MessageID   string `json:"MessageID"`
-			MessageHref string `json:"MessageHref"`
-		} `json:"To"`
-	} `json:"Messages"`
-}
-
-func rangeIn(low, hi int) int {
-	return low + rand.Intn(hi-low)
-}
-
 func main() {
-	secretLength := 4
-	code := gotp.RandomSecret(secretLength)
-
-	var jsonData = []byte(`{
-		"Messages":[
-				{
-						"From": {
-								"Email": "gozza15bdg@gmail.com",
-								"Name": "Fadillah"
-						},
-						"To": [
-								{
-										"Email": "` + "gozzafadillah@gmail.com" + `",
-										"Name": "` + "aziz" + `"
-								}
-						],
-						"Subject": "Cek Ombak",
-						"TextPart": "Code Generator",
-						"HTMLPart": "<center><h2>OTP Code</h2><br /> <b><u>` + code + `</u></b> </center>"
-				}
-		]
-	}`)
-
-	url := "https://api.mailjet.com/v3.1/send"
-
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-
-	basic := base64.StdEncoding.EncodeToString([]byte("177a3a51988d43f5512cf71bff810623" + ":" + "ba69cb7437c1bad179c8af199ba33dd1"))
-
-	req.Header.Add("Authorization", "Basic "+basic)
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	var result = respon{}
-	json.Unmarshal(body, &result)
-
-	fmt.Println(result)
+	text := slug.Make("E-wallet Dana")
+	fmt.Println(text)
 }
