b4443fa1e416b3f54bdbf1a2e17579b7e937c42d
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-15 23:09:22 +0700

    blom jadi

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module gozzafadillah
 
 go 1.17
 
-require github.com/xlzd/gotp v0.0.0-20220110052318-fab697c03c2c
-
-require (
-	github.com/gosimple/slug v1.12.0 // indirect
-	github.com/gosimple/unidecode v1.0.1 // indirect
-)
+require github.com/mailjet/mailjet-apiv3-go v0.0.0-20201009050126-c24bc15a9394
--- main.go
+++ main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/gosimple/slug"
+	. "github.com/mailjet/mailjet-apiv3-go"
 )
 
 func main() {
-	text := slug.Make("E-wallet Dana")
-	fmt.Println(text)
+	mailjetClient := NewMailjetClient("83a241161ed94c0d8668ce984a624a4d", "76cc38cf83e14ef68477ecdd643d5363")
+	email := &InfoSendMail{
+		FromEmail: "gozza15bdg@gmail.com",
+		FromName:  "Mailjet Pilot",
+		Subject:   "Your email flight plan!",
+		TextPart:  "Dear passenger, welcome to Mailjet! May the delivery force be with you!",
+		HTMLPart:  "<h3>Dear passenger, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!<br />May the delivery force be with you!",
+		Recipients: []Recipient{
+			Recipient{
+				Email: "gozzafadillah@gmail.com",
+			},
+		},
+	}
+	res, err := mailjetClient.SendMail(email)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Success")
+		fmt.Println(res)
+	}
 }
