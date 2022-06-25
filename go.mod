4f0191867e7835a12f98334a6a38315af1171fab
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-25 21:39:36 +0700

    xendit api

--- go.mod
+++ go.mod
@@ -2,11 +2,24 @@ module gozzafadillah
 
 go 1.17
 
-require github.com/xendit/xendit-go v1.0.8
+require (
+	github.com/google/uuid v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/xendit/xendit-go v1.0.8
+)
 
 require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.2.0 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -4,25 +4,73 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/google/uuid"
+	"github.com/labstack/echo"
 	"github.com/xendit/xendit-go"
-	"github.com/xendit/xendit-go/retailoutlet"
+	"github.com/xendit/xendit-go/invoice"
 )
 
 func main() {
+	e := echo.New()
+	e.GET("/data", xendit_invoice)
+	e.Start(":8080")
+
+}
+
+func xendit_invoice(ctx echo.Context) error {
+
 	xendit.Opt.SecretKey = "xnd_development_I0guK5bOcQB3AVQ8pYUXMtXltsVvfqsyPU2dz1RJvTDNVrsLVxqC8K0KJc3YhlZE"
+	code := uuid.New()
 
-	data := retailoutlet.CreateFixedPaymentCodeParams{
-		ExternalID:       "retailoutlet-external-id",
-		RetailOutletName: xendit.RetailOutletNameAlfamart,
-		Name:             "Michael Jackson",
-		ExpectedAmount:   200000,
+	customer := xendit.Customer{
+		GivenNames:   "John",
+		Surname:      "Doe",
+		Email:        "gozzafadillah@gmail.com",
+		MobileNumber: "+62895631948686",
+	}
+	invoiceCustomer := xendit.InvoiceCustomer{
+		GivenNames:   customer.GivenNames,
+		Email:        customer.Email,
+		MobileNumber: customer.MobileNumber,
+	}
+	item := xendit.InvoiceItem{
+		Name:     "Paket XL 20RB",
+		Price:    20000,
+		Quantity: 1,
+		Category: "Top Up",
+	}
+	fee := xendit.InvoiceFee{
+		Type:  "ADMIN",
+		Value: 2000,
 	}
 
-	resp, err := retailoutlet.CreateFixedPaymentCode(&data)
+	NotificationType := []string{"whatsapp", "email", "sms"}
+	customerNotificationPreference := xendit.InvoiceCustomerNotificationPreference{
+		InvoiceCreated:  NotificationType,
+		InvoiceReminder: NotificationType,
+		InvoicePaid:     NotificationType,
+		InvoiceExpired:  NotificationType,
+	}
+
+	data := invoice.CreateParams{
+		ExternalID:                     "demo_" + code.String(),
+		Amount:                         item.Price + fee.Value,
+		PayerEmail:                     customer.Email,
+		Description:                    "Paket Data XL",
+		Items:                          []xendit.InvoiceItem{item},
+		Customer:                       invoiceCustomer,
+		Fees:                           []xendit.InvoiceFee{fee},
+		CustomerNotificationPreference: customerNotificationPreference,
+	}
+
+	resp, err := invoice.Create(&data)
 	if err != nil {
-		log.Fatal(err.ErrorCode)
+		log.Fatal(err)
 	}
 
-	fmt.Printf("created retail outlet fixed payment code: %+v\n", resp)
-
+	fmt.Printf("created invoice: %+v\n", resp)
+	return ctx.JSON(200, map[string]interface{}{
+		"message": "success",
+		"payment": resp,
+	})
 }
