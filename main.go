79e80d3edfad32dd9c0f43f5221ed9950e7add45
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-07-26 14:49:20 +0700

    salah logic

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func Sum(data []int) int {
+	hasil := 0
+	for i := 0; i < len(data); i++ {
+		hasil += data[i]
+	}
+	return hasil
+}
+
+func CekEven(number int) bool {
+	if number%2 != 0 {
+		return false
+	}
+	return true
+}
 
 func main() {
-	fmt.Println("hello world")
+	data := []int{1, 3, 5, 7}
+	// sort array
+	sort.Slice(data, func(i, j int) bool { return data[i] < data[j] })
+	fmt.Println(data)
+	// hitung jarak antar data array
+	counter := 0
+	dataCounter := []int{}
+	for j := 1; j <= len(data)-1; j++ {
+		counter = data[j] - data[j-1]
+		dataCounter = append(dataCounter, counter)
+	}
+	// menambah semua selisih
+	sum := Sum(dataCounter)
+	fmt.Println("data counter :", dataCounter)
+	fmt.Println("data sum :", sum)
+
+	// Cek total itu genap
+	status := CekEven(sum)
+	fmt.Println("Even ? :", status)
+
 }
