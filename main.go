ba859834884e7d52a8863944320a162efeda90f2
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-08-08 00:00:48 +0700

    end

--- main.go
+++ main.go
@@ -1,44 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-func Sum(data []int) int {
-	hasil := 0
-	for i := 0; i < len(data); i++ {
-		hasil += data[i]
-	}
-	return hasil
-}
-
-func CekEven(number int) bool {
-	if number%2 != 0 {
-		return false
-	}
-	return true
-}
+import "fmt"
 
 func main() {
-	data := []int{1, 3, 5, 7}
-	// sort array
-	sort.Slice(data, func(i, j int) bool { return data[i] < data[j] })
+	data := "bat"
+	lenght := len(data)
+	var str = data[lenght-2 : lenght]
+	data = Reverse(str)
+	data = string(data[0]) + " " + string(data[1])
 	fmt.Println(data)
-	// hitung jarak antar data array
-	counter := 0
-	dataCounter := []int{}
-	for j := 1; j <= len(data)-1; j++ {
-		counter = data[j] - data[j-1]
-		dataCounter = append(dataCounter, counter)
+}
+
+func Reverse(str string) string {
+	runeString := []rune(str)
+	for i, j := 0, len(runeString)-1; i < j; i, j = i+1, j-1 {
+		runeString[i], runeString[j] = runeString[j], runeString[i]
 	}
-	// menambah semua selisih
-	sum := Sum(dataCounter)
-	fmt.Println("data counter :", dataCounter)
-	fmt.Println("data sum :", sum)
 
-	// Cek total itu genap
-	status := CekEven(sum)
-	fmt.Println("Even ? :", status)
-
+	return string(runeString)
 }
